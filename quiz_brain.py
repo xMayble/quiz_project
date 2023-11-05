74b86f6e0cae3f556da1dce3660837b68b4451f7
Mayble <mehburohit@gmail.com>
2023-11-04 22:58:43 -0400

    implemented function to check user's accuracy

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -11,3 +12,15 @@ class QuizBrain:
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, question_answer):
+        if user_answer.lower() == question_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {question_answer}")
+        print(f"The current score is: {self.score}/{self.question_number}")
+            
+            
